d272e79341a9d184de0c2b177a0366e71a3ac647
Justin Harr <justin@Justins-MBP.lan>
2019-06-04 23:02:27 -0400

    remove app root test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Suspense, lazy } from "react";
+import "./assets/scss/App.scss";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
 
-const AppRoot = lazy(() => import("./pages/AppRoot"));
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-export default () => (
-  <Suspense
-    fallback={
-      <div className="loader-wrapper">
-        <div class="loader" />
-      </div>
-    }
-  >
-    <AppRoot />
-  </Suspense>
-);
+const Contact = lazy(() => import("./pages/Contact"));
+const Resume = lazy(() => import("./pages/Resume"));
+
+const App = () => {
+  return (
+    <Router>
+      <Suspense
+        fallback={
+          <div className="loader-wrapper">
+            <div class="loader" />
+          </div>
+        }
+      >
+        <div>
+          <Nav />
+          <Route path="/" exact component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} />
+          <Footer />
+        </div>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default App;
